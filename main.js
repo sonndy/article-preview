08c6228d3768684e3bd984ad02955b2d10f68f19
sonndy <123dykai@gmail.com>
2021-07-10 17:54:57 -0400

    creates interactive share social menu

--- main.js
+++ main.js
@@ -5,10 +5,26 @@ window.addEventListener("DOMContentLoaded", () => {
    nameDate.addEventListener("click", showSocial);
 
    function showSocial() {
+        nameDate.classList.toggle("btn-color");
+        let btnColor = document.querySelector(".sharebutton");
+        btnColor.classList.toggle("filer")
        let parentDiv = this.parentElement;
+       let resetTriangle = document.querySelector(".triangle");
+
        let socialOverlay = document.querySelector(".new-share");
        parentDiv.classList.toggle("hide");
        socialOverlay.classList.toggle("show");
+
+       if(resetTriangle !== null)
+       {
+           resetTriangle.remove();
+       } else {
+            let triangle = document.createElement("div");
+             triangle.classList.add("triangle");
+             socialOverlay.parentNode.insertBefore(triangle, socialOverlay.nextSibling);
+       }
+       
+       
    }
 
    let socials = document.querySelector(".new-share");
@@ -16,7 +32,8 @@ window.addEventListener("DOMContentLoaded", () => {
    socials.addEventListener("click", hideSocial);
 
    function hideSocial(){
-       let parentDiv = this.parentElement.lastElementChild;
+       let parentDiv = document.querySelector(".new-share");
+       console.log(parentDiv)
        let newName = document.querySelector(".contacts");
        parentDiv.classList.toggle("show");
 
